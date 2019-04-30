e23567dc34867d5abfaefcf8c80ec3d0951c6aeb
Christopher Ly <38119369+CrossMultiply16@users.noreply.github.com>
2019-04-30 12:46:41 -0700

    Update upperCaseifier.h

--- source/upperCaseifier.h
+++ source/upperCaseifier.h
@@ -1,4 +1,6 @@
 //Makes the letter inserted into uppercase so functions can read it
+#ifndef UPPERCASEIFIER_H
+#define UPPERCASEIFIER_H
 string upperCaseifier(string s){
 	for(int x = 0; x < s.length(); x++){
 		s[x] = toupper(s[x]);
@@ -6,3 +8,4 @@ string upperCaseifier(string s){
 
 	return s;
 }
+#endif

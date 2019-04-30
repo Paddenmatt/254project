72efb66212df55a6e2f92dfddbc96d3dac24f00f
Derrick <derricklee@csu.fullerton.edu>
2019-04-29 23:46:31 -0700

    Updated display.h to output visual solution and guesses

--- source/display.h
+++ source/display.h
@@ -1,31 +1,59 @@
 #include <iostream>
 #include <string>
 
-void displaysolution(int arr[]) {
-    for(int i=0; i<arr.size();i++){
-        if(arr[i]=='R'){
+void display(string guess) {
+	cout << "Your guess: \n";
+	for (int i = 0; i < guess.length(); i++) {
+		char color = guess[i];
+		switch(toupper(color)) 
+		{
+			case 'R': cout << ansi::foreground::RED << "\u2B24 ";
+					break;
 
-        }
-        else if(arr[i]=='W'){
+			case 'W': cout << ansi::foreground::WHITE << "\u2B24 ";
+					break;
 
-        }
-        else if(arr[i]=='G'){
-            
-        }
-        else if(arr[i]=='Y'){
-            
-        }
-        else if(arr[i]=='B'){
-            
-        }
-        else if(arr[i]=='M'){
-            
-        }
-    }
- 
- return;
+			case 'Y': cout << ansi::foreground::YELLOW << "\u2B24 ";
+					break;
+
+			case 'G': cout << ansi::foreground::GREEN << "\u2B24 ";
+					break;
+
+			case 'B': cout << ansi::foreground::BLUE << "\u2B24 ";
+					break;
+
+			case 'M': cout << ansi::foreground::MAGENTA << "\u2B24 ";
+					break;
+		}
+	}
+	cout<< ansi::RESET << endl << endl;
 }
-void displayguess(string word) {
 
-return;
+void displaySol(char sol[]) {
+	cout << "The correct answer is: \n";
+	char color;
+	for (int i = 0; i < 4; i++) {
+		color = sol[i];
+		switch(toupper(color)) 
+		{
+			case 'R': cout << ansi::foreground::RED << "\u2B24 ";
+					break;
+
+			case 'W': cout << ansi::foreground::WHITE << "\u2B24 ";
+					break;
+
+			case 'Y': cout << ansi::foreground::YELLOW << "\u2B24 ";
+					break;
+
+			case 'G': cout << ansi::foreground::GREEN << "\u2B24 ";
+					break;
+
+			case 'B': cout << ansi::foreground::BLUE << "\u2B24 ";
+					break;
+
+			case 'M': cout << ansi::foreground::MAGENTA << "\u2B24 ";
+					break;
+		}
+	}
+	cout<< ansi::RESET << endl << endl;
 }
